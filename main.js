7b09f353426d631bbc15eda30e76901b314dbe83
Diegoanjos86 <falecom.diegoanjos@gmail.com>
2025-03-13 15:08:33 -0300

    Conectando JavaScript ao HTML

---
+++ main.js
@@ -0,0 +1,11 @@
+const conect_log = ()=> console.log("Conexão com Java script realizada com sucesso ")
+const conect_alert = ()=> alert("Conexão com Java script realizada com sucesso ")
+
+conect_log()
+conect_alert()
+
+conect_log()
+conect_alert()
+
+conect_log()
+conect_alert()
